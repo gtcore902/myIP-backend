19cc547da444f06452650ded82411f7f5f04be51
gtcore902 <gaetan.tremois@gmail.com>
2024-04-18 14:10:08 +0200

    perf: make the request asynchronous

--- controllers/getIp.js
+++ controllers/getIp.js
@@ -1,11 +1,22 @@
 const getIP = require('external-ip')();
 
-exports.getIp = (req, res, next) => {
-  getIP((err, ip) => {
-    if (err) {
-      res.status(404).json({ err });
-      throw err;
-    }
+// exports.getIp = (req, res, next) => {
+//   getIP((err, ip) => {
+//     if (err) {
+//       res.status(404).json({ err });
+//       throw err;
+//     }
+//     res.status(200).json({ ip });
+//   });
+// };
+
+exports.getIp = async (req, res, next) => {
+  try {
+    // Attempts to retrieve external IP address asynchronously
+    const ip = await getIP.get();
     res.status(200).json({ ip });
-  });
+  } catch (err) {
+    console.error('Error retrieving external IP address:', err);
+    res.status(500).json({ error: 'Unable to retrieve external IP address' });
+  }
 };
--- routes/getIp.js
+++ routes/getIp.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 const getIpCtrl = require('../controllers/getIp');
+
+router.use(bodyParser.json());
 
 router.get('/', getIpCtrl.getIp);
 
